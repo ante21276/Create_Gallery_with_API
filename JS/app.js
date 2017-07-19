a45afeec14b4a1c95d5bce0233485a0d7240c701
ante <ante@gmail.com>
2017-07-19 18:00:44 +0200

    Added countries

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-  let url = "https://restcountries.eu/rest/v2/all";
+  let $li = $("#region li");
+  let countriesAPI = "https://restcountries.eu/rest/v2/all";
+  let html = "<ul>";
+  $.getJSON(countriesAPI, function(data) {
+      $.each(data, function(index, country) {
+        html += "<li>" + country.name + "</li>";
+      }); //each
+      html += "</ul>"
+      $("#main").html(html)
 
-  $.getJSON(url, function(data) {
-    $.each(data, function(index, value) {
-      if(value.name === "Croatia") {
-        console.log(value)
-      }
-    });
-  });
-}); //Ready DOM
+      $li.click(function(e) {
+        $("#main li").hide()
+        // if()
+        // $.each(data, function(index, country) {
+        //   html += "<li>" + country.name + "</li>";
+        // }); //each
+        // html += "</ul>"
+        // $("#main").html(html)
+
+      });
+  });  //JSON
+
+
+
+})
