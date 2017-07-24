4bb5199ad607e2f1dd04bd066d29f5e6ad50d16d
ante <ante@gmail.com>
2017-07-24 10:55:06 +0200

    DRY

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,18 @@
-
   const countriesAPI = "https://restcountries.eu/rest/v2/all";
   let html = "<ul id='countriesList'>";
+  let overlayInfo = "";
+  function country1 (overlayInfo, country) {
+    overlayInfo += "<img  class='svg'  src='https://restcountries.eu/data/" + country.alpha3Code.toLowerCase()  +".svg' >";
+    overlayInfo += '<p> Country:\xa0\xa0' + country.name + '</p>';
+    overlayInfo += "<p> Region:\xa0\xa0" + country.region + "</p>";
+    overlayInfo += "<p> Subregion:\xa0\xa0" + country.subregion + "</p>";
+    overlayInfo += "<p> Capital:\xa0\xa0" + country.capital + "</p>";
+    overlayInfo += "<p> Population:\xa0\xa0" + country.population.toLocaleString() + "</p>";
+    overlayInfo += "<p> Area:\xa0\xa0" + country.area.toLocaleString() + " km</p>";
+    overlayInfo += "<p> Native Name:\xa0\xa0" + country.nativeName + "</p>";
+
+  }
+
   $.getJSON(countriesAPI, function(data) {
       $.each(data, function(index, country) {
         html += "<li data-internalid=" + index + " class=" + country.region + ">";
@@ -10,12 +22,13 @@
       $("#main").html(html);
 
       let $li = $("#countriesList li");
-
       $li.click(function(e) {
+
         let overlayInfo = "";
          $.each(data, function(index, country) {
 
            if(e.target.innerHTML === country.name) {
+             overlayInfo += "<img  class='svg'  src='https://restcountries.eu/data/" + country.alpha3Code.toLowerCase()  +".svg' >";
              overlayInfo += '<p> Country:\xa0\xa0' + country.name + '</p>';
              overlayInfo += "<p> Region:\xa0\xa0" + country.region + "</p>";
              overlayInfo += "<p> Subregion:\xa0\xa0" + country.subregion + "</p>";
@@ -33,40 +46,47 @@
         let next = document.getElementById("next");
 
          prev.addEventListener("click", () => {
-
            atribute -= 1;
+           if (atribute < 1) {
+             atribute = 249
+           }
 
-           let overlayInfo1 = "";
+           let overlayInfo = "";
            $.each(data, function(index, country) {
              if (atribute === index) {
-               overlayInfo1 += "<p> Country:\xa0\xa0" + country.name + "</p>";
-               overlayInfo1 += "<p> Region:\xa0\xa0" + country.region + "</p>";
-               overlayInfo1 += "<p> Subregion:\xa0\xa0" + country.subregion + "</p>";
-               overlayInfo1 += "<p> Capital:\xa0\xa0" + country.capital + "</p>";
-               overlayInfo1 += "<p> Population:\xa0\xa0" + country.population.toLocaleString() + "</p>";
-               overlayInfo1 += "<p> Area:\xa0\xa0" + country.area.toLocaleString() + " km</p>";
-               overlayInfo1 += "<p> Native Name:\xa0\xa0" + country.nativeName + "</p>";
+               overlayInfo += "<img  class='svg'  src='https://restcountries.eu/data/" + country.alpha3Code.toLowerCase()  +".svg' >";
+               overlayInfo += "<p> Country:\xa0\xa0" + country.name + "</p>";
+               overlayInfo += "<p> Region:\xa0\xa0" + country.region + "</p>";
+               overlayInfo += "<p> Subregion:\xa0\xa0" + country.subregion + "</p>";
+               overlayInfo += "<p> Capital:\xa0\xa0" + country.capital + "</p>";
+               overlayInfo += "<p> Population:\xa0\xa0" + country.population.toLocaleString() + "</p>";
+               overlayInfo += "<p> Area:\xa0\xa0" + country.area.toLocaleString() + " km</p>";
+               overlayInfo += "<p> Native Name:\xa0\xa0" + country.nativeName + "</p>";
              }
            });
-           $("#text").html(overlayInfo1);
+           $("#text").html(overlayInfo);
            document.getElementById("overlay").style.display = "block";
         });
 
         next.addEventListener("click", () => {
           atribute += 1;
-          let overlayInfo2 = "";
+          if (atribute > 249) {
+            atribute = 0
+          }
+          let overlayInfo = "";
           $.each(data, function(index, country) {
             if (atribute === index) {
-              overlayInfo2 += "<p> Country:\xa0\xa0" + country.name + "</p>";
-              overlayInfo2 += "<p> Region:\xa0\xa0" + country.region + "</p>";
-              overlayInfo2 += "<p> Subregion:\xa0\xa0" + country.subregion + "</p>";
-              overlayInfo2 += "<p> Capital:\xa0\xa0" + country.capital + "</p>";
-              overlayInfo2 += "<p> Population:\xa0\xa0" + country.population.toLocaleString() + "</p>";
-              overlayInfo2 += "<p> Area:\xa0\xa0" + country.area.toLocaleString() + " km</p>";
-              overlayInfo2 += "<p> Native Name:\xa0\xa0" + country.nativeName + "</p>";
+              overlayInfo += "<img  class='svg'  src='https://restcountries.eu/data/" + country.alpha3Code.toLowerCase()  +".svg' >";
+              overlayInfo += "<p> Country:\xa0\xa0" + country.name + "</p>";
+              overlayInfo += "<p> Region:\xa0\xa0" + country.region + "</p>";
+              overlayInfo += "<p> Subregion:\xa0\xa0" + country.subregion + "</p>";
+              overlayInfo += "<p> Capital:\xa0\xa0" + country.capital + "</p>";
+              overlayInfo += "<p> Population:\xa0\xa0" + country.population.toLocaleString() + "</p>";
+              overlayInfo += "<p> Area:\xa0\xa0" + country.area.toLocaleString() + " km</p>";
+              overlayInfo += "<p> Native Name:\xa0\xa0" + country.nativeName + "</p>";
             }
           });
-          $("#text").html(overlayInfo2);
+          $("#text").html(overlayInfo);
           document.getElementById("overlay").style.display = "block";
        });
 
